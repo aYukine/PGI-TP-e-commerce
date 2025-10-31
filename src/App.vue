2ef274bb77a26485ed315fd76154c5502110fccb
aYukine <phayuk168@gmail.com>
2025-10-31 18:03:13 +0700

    Done Ex3

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div id="app-root">This is my first VueJs project</div>
+</template>
+
+<style scoped>
+#app-root {
+  font-family: Inter, ui-sans-serif, system-ui, -apple-system, "Segoe UI", Roboto, "Helvetica Neue", Arial;
+  font-size: 1.25rem;
+  text-align: center;
+  margin: 3rem 1rem;
+}
+</style>
