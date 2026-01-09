ceae219b167a7645361730ebcf4101e868c786a0
aYukine <phayuk168@gmail.com>
2026-01-09 14:21:59 +0700

    added passport

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -2,27 +2,46 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 use \App\Http\Controllers\CategoryController;
 use \App\Http\Controllers\ProductController;
+
+Route::post('/login', function (Request $request) {
+    $request->validate(['email'=>'required|email','password'=>'required']);
+
+    if (!Auth::attempt($request->only('email','password'))) {
+        return response()->json(['message'=>'Invalid credentials'], 401);
+    }
+
+    $user = $request->user();
+    $token = $user->createToken('mobile')->accessToken;
+
+    return response()->json(['token'=>$token]);
+});
+
+Route::middleware('auth:api')->group(function () {
+    Route::get('/me', function(Request $request) {
+        return $request->user()->load('roles');
+    });
+
+    Route::controller(CategoryController::class)->prefix('categories')->group(function () {
+        Route::get('/', 'getCategories');
+        Route::post('/', 'createCategory');
+        Route::get('/{categoryId}', 'getCategory');
+        Route::patch('/{categoryId}', 'updateCategory');
+        Route::delete('/{categoryId}', 'deleteCategory');
+    });
+
+    Route::controller(ProductController::class)->prefix('products')->group(function () {
+        Route::get('/', 'getProducts');
+        Route::post('/', 'createProduct');
+        Route::get('/{productId}', 'getProduct');
+        Route::patch('/{productId}', 'updateProduct');
+        Route::delete('/{productId}', 'deleteProduct');
+        Route::get('/{categoryId}/products', 'getProductsByCategory');
+    });
+});
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
-
-
-Route::controller(CategoryController::class)->prefix('categories')->group(function () {
-    Route::get('/', 'getCategories');
-    Route::post('/', 'createCategory');
-    Route::get('/{categoryId}', 'getCategory');
-    Route::patch('/{categoryId}', 'updateCategory');
-    Route::delete('/{categoryId}', 'deleteCategory');
-});
-
-Route::controller(ProductController::class)->prefix('products')->group(function () {
-    Route::get('/', 'getProducts');
-    Route::post('/', 'createProduct');
-    Route::get('/{productId}', 'getProduct');
-    Route::patch('/{productId}', 'updateProduct');
-    Route::delete('/{productId}', 'deleteProduct');
-    Route::get('/{categoryId}/products', 'getProductsByCategory');
-});
