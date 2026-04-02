4e16c55e58f2908a4e600c4ee1f2aecedf3c9e88
aYukine <phayuk168@gmail.com>
2026-04-02 22:32:00 +0700

    created a circular dependency

--- tp03/src/notifications/notifications.module.ts
+++ tp03/src/notifications/notifications.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { NotificationsService } from './notifications.service';
+import { OrdersModule } from 'src/orders/orders.module';
 
 @Module({
+  imports: [OrdersModule],
   providers: [NotificationsService],
   exports: [NotificationsService],
 })
--- tp03/src/notifications/notifications.service.ts
+++ tp03/src/notifications/notifications.service.ts
@@ -1,7 +1,10 @@
 import { Injectable } from '@nestjs/common';
+import { OrdersService } from 'src/orders/orders.service';
 
 @Injectable()
 export class NotificationsService {
+  constructor(private readonly ordersService: OrdersService) {}
+
   notify(event: string, payload: any) {
     // For lab: just log
     console.log(`[NOTIFY] ${event}`, payload);
