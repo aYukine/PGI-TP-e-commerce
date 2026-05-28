e2555f7ff9862c95cb40f704782c327b729d886d
aYukine <phayuk168@gmail.com>
2026-05-28 17:50:25 +0700

    Checkpoint part D. added apollo client and registered pinia and apollo in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
-import { createApp } from 'vue'
+import { createApp, h, provide } from 'vue'
 import { createPinia } from 'pinia'
+import { DefaultApolloClient } from '@vue/apollo-composable'
 import App from './App.vue'
+import { apolloClient } from './apollo/client'
 
-const app = createApp(App)
+const app = createApp({
+  setup() {
+    provide(DefaultApolloClient, apolloClient)
+  },
+  render: () => h(App),
+})
 
 app.use(createPinia())
-
 app.mount('#app')
